358e907443e4176b3434377a6003b64551c37c8d
Michael Langfermann <versable@gmail.com>
2016-01-03 18:30:44 +0100

    Implement streaming from filmpalast

--- sites/filmpalast_to.py
+++ sites/filmpalast_to.py
@@ -14,7 +14,7 @@ SITE_NAME = 'FilmPalast.to'
 SITE_ICON = 'filmpalast.png'
 
 URL_MAIN = 'http://www.filmpalast.to/'
-URL_STREAM = URL_MAIN + 'stream/{id}/1'
+URL_STREAM = URL_MAIN + 'stream/%d/1'
 URL_MOVIES_NEW = URL_MAIN + 'movies/new'
 URL_MOVIES_TOP = URL_MAIN + 'movies/top'
 URL_SHOWS_NEW = URL_MAIN + 'serien/view'
@@ -31,12 +31,12 @@ def load():
     oGui.addFolder(cGuiElement('Top Filme', SITE_IDENTIFIER, 'showEntries'), params)
     oGui.setEndOfDirectory()
 
-
 def showEntries():
     oGui = cGui()
-    oGui.setView('movie')
     params = ParameterHandler()
-    oRequestHandler = cRequestHandler(params.getValue('sUrl'))
+    entryUrl = params.getValue('sUrl')
+    oRequestHandler = cRequestHandler(entryUrl)
+    oGui.setView('tvshows' if 'serien/' in entryUrl else 'movie')
     sHtmlContent = oRequestHandler.request()
     # Grab the link and title
     pattern = '<a[^>]*href="([^"]*)"[^>]*title="([^"]*)"[^>]*>[^<]*'
@@ -49,11 +49,12 @@ def showEntries():
         logger.info(sThumbnail)
         oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showHosters')
         oGuiElement.setMediaType('movie')
-        oGuiElement.setThumbnail(sThumbnail if 'http:' in sThumbnail else URL_MAIN + sThumbnail)
-        params.setParam('entryUrl', sUrl)
+        oGuiElement.setThumbnail(__checkUrl(sThumbnail))
+        params.setParam('entryUrl', __checkUrl(sUrl))
         oGui.addFolder(oGuiElement, params, bIsFolder = False)
 
-    pattern = '<a[^>]*class="[^"]*pageing[^"]*"[^>]*href=["\']([^"\']*)["\'][^>]*>[ ]*vorw'
+    pattern = '<a[^>]*class="[^"]*pageing[^"]*"[^>]*'
+    pattern += 'href=["\']([^"\']*)["\'][^>]*>[ ]*vorw'
     aResult = cParser().parse(sHtmlContent, pattern)
     if aResult[0]:
         logger.info(aResult[1][0])
@@ -63,4 +64,44 @@ def showEntries():
 
 # Show the hosters dialog
 def showHosters():
-    return
+    params= ParameterHandler()
+    logger.info(params.getValue('entryUrl'))
+    oRequestHandler = cRequestHandler(params.getValue('entryUrl'))
+    sHtmlContent = oRequestHandler.request()
+    pattern = '<p[^>]*class="hostName"[^>]*>([^<>]+)</p>.*?'
+    pattern += '<a[^>]*class="[^"]*stream-src[^"]*"[^>]*data-id="([^"]+)"[^>]*>'
+    aResult = cParser().parse(sHtmlContent, pattern)
+    if not aResult[0]:
+        return
+    hosters = []
+    for sHost, sId in aResult[1]:
+        hoster = dict()
+        hoster['link'] = __getSource(sId)
+        hoster['name'] = sHost
+        hoster['displayedName'] = sHost
+        hosters.append(hoster)
+    if hosters:
+        hosters.append('getHosterUrl')
+    return hosters
+
+def getHosterUrl(sUrl = False):
+    oParams = ParameterHandler()
+    if not sUrl:
+        sUrl = oParams.getValue('url')
+    results = []
+    result = {}
+    result['streamUrl'] = sUrl
+    result['resolved'] = False
+    results.append(result)
+    return results
+
+def __checkUrl(url):
+    return url if 'http:' in url else URL_MAIN + url
+
+def __getSource(id):
+    url = URL_STREAM % int(id)
+    from t0mm0.common.net import Net
+    idValue = { 'streamID' : id }
+    headValue = { 'Referer' : URL_MAIN, 'X-Requested-With':'XMLHttpRequest' }
+    data = Net().http_POST(url, idValue, headValue).content
+    return re.compile('"url":"([^"]+)"').findall(data)[0].replace('\\', '')
